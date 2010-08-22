cfa20b17814e739865e9f01af35bbe54860c4d0c
Yoav Ram <yoavram+github@gmail.com>
2010-08-22 09:37:56 +0000

    rename

---
+++ src/il/ac/tau/yoavram/simba/statistics/aggregators/StandardDeviationMutationRate.java
@@ -0,0 +1,13 @@
+package il.ac.tau.yoavram.simba.statistics.aggregators;
+
+import il.ac.tau.yoavram.pes.statistics.aggregators.StandardDeviation;
+import il.ac.tau.yoavram.simba.Bacteria;
+
+public class MutationRateStandardDeviation extends StandardDeviation<Bacteria> {
+
+	@Override
+	protected double extractData(Bacteria input) {
+		return input.getMutationRate();
+	}
+
+}
