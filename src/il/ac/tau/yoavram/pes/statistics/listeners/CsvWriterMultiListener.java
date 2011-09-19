b330e5f89021803904882fcabfc0f35bb6d9195d
Yoav Ram <yoavram+github@gmail.com>
2011-09-19 13:01:33 +0000

    workaround

--- src/il/ac/tau/yoavram/pes/statistics/listeners/CsvWriterMultiListener.java
+++ src/il/ac/tau/yoavram/pes/statistics/listeners/CsvWriterMultiListener.java
@@ -1,10 +1,17 @@
 package il.ac.tau.yoavram.pes.statistics.listeners;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import com.google.common.collect.Lists;
 
+/**
+ * The close method of this class does nothing, as this class is expected to be used with multiple gatherers. 
+ * Therefore one must take care that Spring will call <code>destroy()</code> when the application is over.
+ * @author yoavram
+ *
+ */
 public class CsvWriterMultiListener extends CsvWriterListener {
 	private List<String> header = null;
 	private List<Number> dataList = null;
@@ -26,7 +33,7 @@ public class CsvWriterMultiListener extends CsvWriterListener {
 			writeHeader(header.toArray(new String[0]));
 			firstListen = false;
 		}
-		
+
 		dataList.addAll(Arrays.asList(data));
 
 		if (dataList.size() == header.size()) {
@@ -35,5 +42,15 @@ public class CsvWriterMultiListener extends CsvWriterListener {
 		} else if (dataList.size() > header.size()) {
 			throw new IllegalStateException("More values than headers!");
 		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		// do nothing, because multiple gatherers will use this and you don't
+		// want the first one to close this.
+	}
+
+	public void destroy() throws IOException {
+		super.close();
 	}
 }
