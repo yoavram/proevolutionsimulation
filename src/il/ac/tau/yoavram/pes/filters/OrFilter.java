dc0b20954806b5d2ff65039b326aa7db9b2aab68
Yoav Ram <yoavram+github@gmail.com>
2012-07-19 08:53:19 +0000

    and/or filter takes two filters and does a logical operator on their results new configuration files for stress induced recombination

---
+++ src/il/ac/tau/yoavram/pes/filters/OrFilter.java
@@ -0,0 +1,28 @@
+package il.ac.tau.yoavram.pes.filters;
+
+public class OrFilter<T> implements Filter<T> {
+	protected Filter<T> filter1;
+	protected Filter<T> filter2;
+	
+	@Override
+	public boolean filter(T filtrate) {
+		return getFilter1().filter(filtrate) || getFilter2().filter(filtrate);
+	}
+
+	public Filter<T> getFilter1() {
+		return filter1;
+	}
+
+	public void setFilter1(Filter<T> filter1) {
+		this.filter1 = filter1;
+	}
+
+	public Filter<T> getFilter2() {
+		return filter2;
+	}
+
+	public void setFilter2(Filter<T> filter2) {
+		this.filter2 = filter2;
+	}
+
+}
