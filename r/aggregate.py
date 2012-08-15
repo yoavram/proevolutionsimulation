c0d0132eec94026a48179bb1d15ac1a97926c6b1
Yoav Ram <yoavram+github@gmail.com>
2012-08-15 09:39:25 +0000

    changed to write csv using newly installed pandas

--- r/aggregate.py
+++ r/aggregate.py
@@ -3,10 +3,11 @@ import re
 import os
 import pickle
 import glob
+import pandas
         
 # final.pop.invasion.pop.3.mu.0.003.r.0.003.pi.1001.apl.11.pi.0.tau.10.rho.1.in.0.5.envch.0.00000002.start.false.rb.false.2012-Aug-10_00-12-50-001_IDT.csv
 
-pattern = re.compile(r"^final.(?P<filter>\w+)\.(?P<jobname>[\w-]*)\.pop\.(?P<pop>\d+)\.mu\.(?P<mu>\d\.\d+)\.r\.(?P<r>\d\.\d+)\.pi\.(?P<pi_original>\d+)\.apl\.(?P<apl>\d+)\.pi\.(?P<pi>\d+)\.tau\.(?P<tau>[\.\d]+)\.rho\.(?P<rho>\d+)\.in\.(?P<in>\d\.\d+)\.envch\.(?P<envch>\d\.?\d*)\.start\.(?P<start>[\w]+)\.rb\.(?P<rb>[\w]+)(?:\.(?P<time>\d{4}-\w{3}-\d{1,2}_\d{2}-\d{2}-\d{2}-\d{3}_\w{3})\.(?P<filetype>\w+))?$")
+pattern = re.compile(r"^final.(?P<filter>\w+)\.(?P<jobname>[\w-]*)\.pop\.(?P<pop>\d+)\.mu\.(?P<mu>\d\.\d+)\.r\.(?P<r>\d\.\d+)\.pi\.(?P<pi_original>\d+)\.apl\.(?P<apl>\d+)\.pi\.(?P<pi>\d+)\.tau\.(?P<tau>[\.\d]+)\.rho\.(?P<rho>[\.\d]+)\.in\.(?P<in>\d\.\d+)\.envch\.(?P<envch>\d\.?\d*)\.start\.(?P<start>[\w]+)\.rb\.(?P<rb>[\w]+)(?:\.(?P<time>\d{4}-\w{3}-\d{1,2}_\d{2}-\d{2}-\d{2}-\d{3}_\w{3})\.(?P<filetype>\w+))?$")
 
 os.chdir("/groups/lilach_hadany/yoavram/workspace/proevolutionsimulation/output/")
 folders = glob.glob('invasion*')
@@ -35,9 +36,9 @@ for fold in folders:
     os.chdir("..")
 
 print "Finished collecting record"
-#df = pandas.DataFrame(records)
-fout_name = "aggregation.pickle.gz"
+df = pandas.DataFrame(records)
+fout_name = "aggregation.csv.gz"
 fout = gzip.open(fout_name, 'wb')
-pickle.dump(records, fout)
-print "Written output list of dicts to file in pickle format",fout_name
-
+df.to_csv(fout)
+fout.close()
+print "Written output data frame file in csv format compressed withe gzip",fout_name
