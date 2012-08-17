fcb648b37794be6380d0881179cd1a031bbc4696
Yoav Ram <yoavram+github@gmail.com>
2012-08-17 07:39:44 +0000

    double invasion class for simarba

---
+++ src/il/ac/tau/yoavram/simba/SimarDoubleInvasion.java
@@ -0,0 +1,8 @@
+package il.ac.tau.yoavram.simba;
+
+import il.ac.tau.yoavram.pes.DoubleInvasion;
+
+public class SimarDoubleInvasion extends
+		DoubleInvasion<ModifierBacteria, ModifierBacteria> {
+	
+}
